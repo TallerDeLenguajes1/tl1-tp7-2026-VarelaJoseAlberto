b67b973f254a53cad124e4736309f7fc56b80948
VarelajoseAlberto <varelaj298@gmail.com>
2026-05-26 14:43:58 -0300

    funcion de metodos completos y funcionando

---
+++ Calculadora/Calculadora.cs
@@ -0,0 +1,36 @@
+namespace EspacioCalculadora;
+
+class Calculadora
+{
+    private double dato;
+
+    public void Sumar(double termino)
+    {
+        dato += termino;
+    }
+
+    public void Resta(double termino)
+    {
+        dato -= termino;
+    }
+
+    public void Multiplicar(double termino)
+    {
+        dato *= termino;
+    }
+
+    public void Dividir(double termino)
+    {
+        dato /= termino;
+    }
+
+    public void Limpiar()
+    {
+        dato = 0;
+    }
+
+    public double Resultado
+    {
+        get { return dato; }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using EspacioCalculadora;
+
+Calculadora calc =new Calculadora();
+
+calc.Sumar(10); //dato = 10
+System.Console.WriteLine(calc.Resultado);
+calc.Sumar(10); //dato = 20
+System.Console.WriteLine(calc.Resultado);
+
+calc.Resta(10); //dato = 10
+System.Console.WriteLine(calc.Resultado);
+
+calc.Multiplicar(3); //dato 30
+System.Console.WriteLine(calc.Resultado);
+
+calc.Dividir(10); //datp = 3
+System.Console.WriteLine(calc.Resultado);
+
+calc.Limpiar(); //dato = 0
+
+System.Console.WriteLine(calc.Resultado);
